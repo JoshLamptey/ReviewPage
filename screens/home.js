09fb42926c2aa3b8e98f81cc3513c46eb4b4be3a
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2024-02-14 01:46:59 -1200

    five

---
+++ screens/home.js
@@ -0,0 +1,15 @@
+import {StyleSheet,View,Text} from 'react-native'
+import React from "react";
+
+export default function Home(){
+    return(
+        <View style={style.container}>
+            <Text>Home Page</Text>
+        </View>
+    )
+}
+const style = StyleSheet.create({
+    container:{
+        padding:24
+    }
+})
