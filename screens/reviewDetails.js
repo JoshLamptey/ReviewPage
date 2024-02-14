4ac383b7c136efd9a189a7d5b9f606240a252bc7
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2024-02-14 01:47:25 -1200

    done

---
+++ screens/reviewDetails.js
@@ -0,0 +1,15 @@
+import {StyleSheet,View,Text} from 'react-native'
+import React from "react";
+
+export default function ReviewDetails(){
+    return(
+        <View style={StyleSheet.container}>
+            <Text>Review Details Page</Text>
+        </View>
+    )
+}
+const style = StyleSheet.create({
+    container:{
+        padding:24
+    }
+})
