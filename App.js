49e116a65ce455c8075a11134448dee1a08778b5
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2024-02-18 05:50:30 -1200

    one

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  {useState } from 'react';
+import * as Font from 'expo-font'
+import {AppLoading} from 'expo'
 import Home from './screens/home';
 
 
+function getFonts(){
+  return Font.loadAsync({
+    'roboto-Regular':require('./assets/Font/Roboto-Regular.ttf'),
+    'roboto-medium':require('./assets/Font/Roboto-Medium.ttf'),
+
+  })
+}
+
 export default function App() {
-  return (
+  const  [fontsLoaded,setFontsLoaded] = useState(false)
+
+  if(fontsLoaded){
+      return (
     <Home/>
   );
+  }else{
+    return(
+      <AppLoading
+      startAsync={getFonts()}
+      onFinish={()=> setFontsLoaded(true)}
+      />
+    )
+  }
+
 }
