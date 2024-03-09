7bc06ad8ce8c13cfd7dd7196dbef79c3189b84e2
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2024-03-09 02:35:36 -1200

    new

---
+++ styles/global.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native'
+
+export const  globalStyles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:20
+    },
+    titleText:{ 
+        color:'#333',
+        fontFamily:'Roboto Medium',
+        fontSize: 18
+    },
+    paragraph:{
+        marginVertical:8,
+        lineHeight:20,
+    }
+})
