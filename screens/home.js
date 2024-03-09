136976e9eb45981ca8565e3979f3d023afae6a2e
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2024-03-09 02:36:32 -1200

    done

--- screens/home.js
+++ screens/home.js
@@ -1,21 +1,12 @@
+import React  from 'react'
 import {StyleSheet,View,Text} from 'react-native'
+import {globalStyles} from '../styles/global'
 
 export default function Home(){
     return(
-        <View style={style.container}>
-            <Text style={style.titleText}>I am not going to lie to you the Shege my eyes haf seen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>I am not going to lie to you the Shege my eyes haf seen</Text>
             <Text>This is in default font</Text>
         </View>
     )
 }
-const style = StyleSheet.create({
-    container:{
-        padding:50
-    },
-    titleText:{ 
-        color:'green',
-        fontFamily:'Roboto Medium'
-        
-    }
-
-}) 
