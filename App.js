393a0953940f6f85e7fbe17889b7c744a7e7dfd6
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2024-03-09 02:35:12 -1200

    two

--- App.js
+++ App.js
@@ -1,7 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import  React,{useEffect } from 'react';
-import {useFonts} from 'expo-font'
-import * as SplashScreen from 'expo-splash-screen';
 import Home from './screens/home';
 import About from './screens/about';
 
@@ -9,25 +5,8 @@ import About from './screens/about';
 
 
 export default function App() {  
-  const [FontsLoaded]= useFonts({
-    "Roboto Medium" : require("./assets/Font/Roboto Medium.ttf"),
-    "Roboto Medium" : require("./assets/Font/Roboto Regular.ttf")
-  })
-
-  useEffect(()=>{
-    async function prepare(){
-      await SplashScreen.preventAutoHideAsync()
-    }
-    prepare()
-  },[])
-
-  if(!FontsLoaded){
-    return undefined
-  }else{
-    SplashScreen.hideAsync()
-  }
-
   return(
     <Home/>
   )
 }
+
